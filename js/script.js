ec77352c0561a2a1a2b04c90698051ab502f7159
nej1044 <nej1044@naver.com>
2021-06-13 15:36:33 +0900

    내용 구현, 수정

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ function scrollHandler() {
   } else {
     $('.menu-right button').css('color', 'white');
   }
+
+  // menu display
+  $('section').each(function() {
+    if($(window).scrollTop() >= $(this).position().top) {
+      $(this).find('.vertical-center').animate({top: '0', opacity: '1'}, 800);
+    }
+  })
 };
 
 $(window).on('scroll', scrollHandler);
@@ -15,7 +22,6 @@ $(window).on('scroll', scrollHandler);
 scrollHandler();
 
 // section scroll
-
 $('.menu-right button').on('click', function() {
   var id = $(this).attr('id');
   if (id == "about-btn") {
